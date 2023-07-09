644bee49f015a0e75bd7d1453719d466f437af21
Vinicius <viniaoliveira22@gmail.com>
2023-07-08 22:22:04 -0300

    quase finalizado

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/" , methods = ["GET","POST"])
 def home():
+    if request.method == "POST":
+        nome = request.form.get("nome")
+        senha = request.form.get("senha")
+        if nome == "professor":
+            #request.form["professor"]
+            return render_template("professor.html")
+        elif nome == "aluno":
+            #request.form["aluno"]
+            return render_template("aluno.html")
     return render_template("index.html")
 
 @app.route("/professor")
 def professor():
     return render_template("professor.html")
-
+        
 @app.route("/aluno")
 def aluno():
     return render_template("aluno.html")
